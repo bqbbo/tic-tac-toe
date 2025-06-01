7d7c692a14b5a376b9cce2662511c978f32a8703
bobbo <bobbye23899@gmail.com>
2025-06-01 03:46:23 +0000

    Clickable Squares with Alerts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+function Square({ value }) {
+    function handleClick() {
+        alert(`Square ${value} clicked`);
+    }
 
-export default function App() {
+    return (
+        <button className="square" onClick={handleClick}>
+            {value}
+        </button>
+    );
+}
+
+export default function Board() {
     return (
         <>
-            <button className="square">X</button>
-            <button className="square">X</button>
+            <div className="board-row">
+                <Square value="1" />
+                <Square value="2" />
+                <Square value="3" />
+            </div>
+            <div className="board-row">
+                <Square value="4" />
+                <Square value="5" />
+                <Square value="6" />
+            </div>
+            <div className="board-row">
+                <Square value="7" />
+                <Square value="8" />
+                <Square value="9" />
+            </div>
         </>
     );
 }
