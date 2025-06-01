55c9dda315108d00d3cbbddc767d697618709b0b
bobbo <bobbye23899@gmail.com>
2025-06-01 03:34:49 +0000

    Structure fixes with `create-react-app`

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
-export default function Square() {
-    return <button className="square">X</button>;
+import React from 'react';
+
+export default function App() {
+    return (
+        <>
+            <button className="square">X</button>
+            <button className="square">X</button>
+        </>
+    );
 }
